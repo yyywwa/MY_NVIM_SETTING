ae102081e0367c764c2d9ba79a3a61a1c7d09a64
ike <3214140360@qq.com>
2023-10-18 13:52:44 +0800

    0.1

--- lua/plugins/translator.lua
+++ lua/plugins/translator.lua
@@ -1,60 +1,15 @@
 return {
-	"niuiic/translate.nvim",
+	"voldikss/vim-translator",
 	version = "*",
-	dependencies = {
-		"niuiic/core.nvim",
-	},
 	config = function()
-		require("translate").setup({
-			translate = {
-				{
-					-- use :TransToZH to start this job
-					cmd = "TransToZH",
-					-- shell command
-					-- translate-shell is used here
-					command = "trans",
-					-- shell command args
-					args = function(trans_source)
-						-- trans_source is the text you want to translate
-						return {
-							"-b",
-							"-e",
-							"google",
-							-- use proxy
-							"-x",
-							"http://127.0.0.1:7890",
-							"-t",
-							"zh-CN",
-							-- you can filter translate source here
-							trans_source,
-						}
-					end,
-					-- how to get translate source
-					-- selection | input | clipboard
-					input = "selection",
-					-- how to output translate result
-					-- float_win | notify | clipboard | insert
-					output = { "notify"},
-				},
-				{
-					cmd = "TransToEN",
-					command = "trans",
-					args = function(trans_source)
-						return {
-							"-b",
-							"-e",
-							"google",
-							"-t",
-							"en",
-							trans_source,
-						}
-					end,
-					input = "input",
-					output = { "notify", "clipboard" },
-				},
-			},
-		})
-		vim.keymap.set("v", "<leader>t", "<cmd>TransToZH<CR>")
-		vim.keymap.set("n", "<leader>T", "<cmd>TransToEN<CR>")
+		vim.g.translator_sourcelang = "en"
+		vim.g.translator_targetlang = "zh-CN"
+		vim.g.translator_default_engines = {
+			"bing",
+			"google",
+			"youdao",
+		}
+    vim.g.translator_proxy_url = "socks5://127.0.0.1:7890",
+		Keymap("v", "<leader>t", "<Plug>TranslateV", Opt)
 	end,
 }
