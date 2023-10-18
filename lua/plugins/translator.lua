ec49af3264c5615642dab9bdc05ded0c8479dd35
ike <3214140360@qq.com>
2023-10-18 14:02:33 +0800

    0.1

--- lua/plugins/translator.lua
+++ lua/plugins/translator.lua
@@ -2,14 +2,10 @@ return {
 	"voldikss/vim-translator",
 	version = "*",
 	config = function()
+    vim.g.translator_proxy_url = "socks5://127.0.0.1:7890"
 		vim.g.translator_sourcelang = "en"
 		vim.g.translator_targetlang = "zh-CN"
-		vim.g.translator_default_engines = {
-			"bing",
-			"google",
-			"youdao",
-		}
-    vim.g.translator_proxy_url = "socks5://127.0.0.1:7890",
+    vim.g.translator_default_engines = {"google"}
 		Keymap("v", "<leader>t", "<Plug>TranslateV", Opt)
 	end,
 }
